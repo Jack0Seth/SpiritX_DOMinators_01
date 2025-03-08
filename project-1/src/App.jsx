cf211c83ceaa2fbf665ca8b56458a41e22e858ff
Vibhusha Gangulel <vibhushananayakkara@gmail.com>
2025-03-08 12:26:37 +0530

    updated

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Signup from './Components/Signup'
+import Login from './Components/Login'
 
 function App() {
 
   return (
     <div>
-      <Signup/>
+      <Login/>
     </div>
   )
 }
