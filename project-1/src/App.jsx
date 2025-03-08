606c21f12e03c45b56f2f041102eae5538b39502
Meghana Nuwanthi <139098518+MeghanaNuwanthi@users.noreply.github.com>
2025-03-08 12:10:32 +0530

    new

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 import './App.css'
 import Signup from './Components/Signup'
+import Login from "./Components/Login";
 
 function App() {
 
   return (
     <div>
-      <Signup/>
+      <Login/>
     </div>
   )
 }
