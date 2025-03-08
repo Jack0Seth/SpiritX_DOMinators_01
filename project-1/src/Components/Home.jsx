9ae88bfb2e923c0967289c261f068b1be2549145
Malsha Amandi De Silva <f20030112002@student.cinec.edu>
2025-03-08 12:18:37 +0530

    assets

--- project-1/src/Components/Home.jsx
+++ project-1/src/Components/Home.jsx
@@ -5,11 +5,11 @@ export default function WelcomePage({ username }) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#1a1325] text-white">
       <h2 className="text-lg font-semibold">WELCOME {username}!</h2>
-      <img src="/spiritx-logo.png" alt="SpiritX Logo" className="mt-4 h-16" />
+      <img src="./Assets/spiritx-logo.png" alt="SpiritX Logo" className="mt-4 h-16" />
       <Button className="mt-6 px-6 py-3 text-lg bg-pink-300 text-black flex items-center space-x-2 hover:bg-pink-400">
         <LogOut className="w-5 h-5 mr-2" /> Logout
       </Button>
-      <img src="/dominators-logo.png" alt="Dominators Logo" className="mt-10 h-12" />
+      <img src="./Assets/dominators-logo.png" alt="Dominators Logo" className="mt-10 h-12" />
     </div>
   );
 }
