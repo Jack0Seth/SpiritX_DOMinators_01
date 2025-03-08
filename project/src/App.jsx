5489e18f373295dda33711c3d64af0d19ece05c1
Vibhusha Gangulel <vibhushananayakkara@gmail.com>
2025-03-08 18:51:52 +0530

    app.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -4,13 +4,17 @@ import Signup from './Components/Signup'
 import Welcome from './Components/Welcome'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authPage, setAuthPage] = useState("login"); // Default is Login page
 
   return (
-    <>
-      <Welcome/>
-    </>
-  )
+    <div>
+      {authPage === "login" ? (
+        <Login navigateToSignup={() => setAuthPage("signup")} />
+      ) : (
+        <Signup navigateToLogin={() => setAuthPage("login")} />
+      )}
+    </div>
+  );
 }
 
 export default App
