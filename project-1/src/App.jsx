e4d4f83b637c4af4b29eee018618763f0f654327
Meghana Nuwanthi <139098518+MeghanaNuwanthi@users.noreply.github.com>
2025-03-08 12:03:31 +0530

    Update App.jsx

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Signup from './Components/Signup'
 
 function App() {
 
-
   return (
-    <>
-      <div>
-        //code
-      </div>
-    </>
+    <div>
+      <Signup/>
+    </div>
   )
 }
 
